4640279a4de2fdcc6f5001e04099e3f0cc0f9a27
pengchujin <2093959501@qq.com>
2020-10-23 22:15:43 +0800

    add gitignore

--- start.sh
+++ start.sh
@@ -3,7 +3,23 @@ url="$1"
 type="$2"
 domain="$3"
 SubPath=""
+RootPath=""
 mail="qq@qq.com"
+
+if  [ ! "$4" ] ;then
+    SubPath=${RootPath}
+    echo "可以选择文件夹名，默认根目录"
+else
+    RootPath="$4"
+fi
+
+if  [ ! "$5" ] ;then
+    mail=${mail}
+    echo "建议填写邮箱📮"
+else
+    mail="$5"
+fi
+
 case $type in
 	a)
 		./OneList -a "${url}" -s "/onedrive01"
@@ -19,10 +35,14 @@ case $type in
     break
 		;;
 esac
+sed -i "s/\"\/\"/\"\/${RootPath}\"/" config.json
 sed -i "s/onedrive01/${SubPath}/" config.json
 sed -i "s/onedrive02/${SubPath}/" config.json
 sed -i "s/onedrive03/${SubPath}/" config.json
 sed -i "s/example.com/${domain}/" Caddyfile
-echo config
-./OneList -bind 0.0.0.0 -port 8888
-caddy run --config Caddyfile --adapter caddyfile
+echo "------------------------ onelist Config ----------------------------"
+cat config.json
+echo "------------------------ Caddy ----------------------------"
+cat Caddyfile
+nohup caddy run --config Caddyfile --adapter caddyfile &
+./OneList -bind localhost -port 8888
